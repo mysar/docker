623b50b12def9b49cd678776d7fe2430b09ac9d4
xiaoyi <1277615541@qq.com>
2018-12-25 13:09:26 +0800

    萧毅

---
+++ src/main/java/com/snow/docker/DockerApplication.java
@@ -0,0 +1,20 @@
+package com.snow.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        System.out.println("xxxxxxxxxxxx");
+        return "Hello git ssss Docker World";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+}
